df5b76b9fb76472153dd8a6bf425053c9a3ddbf1
SubF <104721940+SubF@users.noreply.github.com>
2022-10-12 21:31:22 -0500

    Add files via upload

--- flask_server/main.py
+++ flask_server/main.py
@@ -1,26 +1,19 @@
 from flask import Flask, render_template, jsonify
-from sqlalchemy import create_engine
-#you can install flash_sqlalchemy using pip install flask-sqlalchemy but neither work (don't as me why it's a dash in the pip install)
-#from flask_sqlalchemy import create_engine
+from flask_sqlalchemy import SQLAlchemy
 
-
+db = SQLAlchemy()
 app = Flask(__name__)
-engine = create_engine("https://my-first-s3-data-bucket-1.s3.amazonaws.com/slope_intercept.csv")
-
+app.config["SQLALCHEMY_DATABASE_URI"]="postgresql://root:Mike0310@mypostgresdb.clwl24wa1fo4.us-east-1.rds.amazonaws.com:5432/my_data_class_db"
+db.init_app(app)
 @app.route('/')
 def home():
-    # tx housing is the table in pgadmin
-    df = pd.read_sql('txhousing', con=engine)
 
-#Is this the correct way to fetch the zip? Can't check it... In our database csv file the zip column is named zipcodes
     return render_template("base.html", zips= ['zipcodes'])
 
 @app.route("/api/<zipcode>")
 def api(zipcode):
-    ret_value = int(zipcode)*2
-    # this is where you SQL read goes
-    #I was trying to look up what we replace the *2 with but I wasn't sure... and I can't get the create_engine to work to throw stuff at the wall through testing.
-    return jsonify({"return":ret_value})
+    ret_value = db.session.execute(f"SELECT * FROM txhousing WHERE zipcodes = {zipcode}")
+    return [(float(x[1]),float(x[2])) for x in ret_value]
 
 if __name__ == "__main__":
     app.run(debug=True)
